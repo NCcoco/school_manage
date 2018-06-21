c9a77778c91775c92b3a2f6cde5bb2dbf0a71774
NC <1872496619@qq.com>
2018-06-21 10:54:11 +0800

    修正

---
+++ school_manage-web/src/main/java/org/nc/school/controller/StudentController.java
@@ -0,0 +1,15 @@
+package org.nc.school.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: StudentController
+ * @Description: 学生逻辑处理
+ * @author nc
+ * @date 2018/6/21 09:53
+ */
+@RestController
+public class StudentController {
+
+
+}
